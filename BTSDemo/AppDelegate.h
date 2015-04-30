9b819ce5d7cb3cb68f4215c32c71ab1b15d480fb
Abdoulaye Diallo <shotone@icloud.com>
2015-04-29 22:26:42 -0400

    Initial Commit

---
+++ BTSDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BTSDemo
+//
+//  Created by A's macAir on 4/29/15.
+//  Copyright (c) 2015 Abdoulaye Diallo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BTSDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BTSDemo
+//
+//  Created by A's macAir on 4/29/15.
+//  Copyright (c) 2015 Abdoulaye Diallo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
